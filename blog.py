26f45ae969043c26dac9562070f28a0ea593b80e
ushvarma <ushvarma@gmail.com>
2022-05-23 00:08:03 -0700

    Added basic HTML to upload and save an image

--- blog.py
+++ blog.py
@@ -1,29 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, redirect
 app = Flask(__name__)
-
-posts = [
-    {
-        'author': 'Harsha',
-        'title' : 'Post 1',
-        'content' : 'Post 1 Content',
-        'posted_on' : 'April 20, 2022'
-    },
-        {
-        'author': 'Varma',
-        'title' : 'Post 2',
-        'content' : 'Post 2 Content',
-        'posted_on' : 'May 20, 2022'
-    }
-]
+app.secret_key = "super secret key"
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html', posts = posts)
+    return render_template('home.html')
 
 @app.route("/about")
 def about():
-    return render_template('about.html', title = 'Happy Blags - About')
+    return render_template('about.html')
+
+@app.route('/upload', methods = ['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect('http://127.0.0.1:5000/')
+        else:
+            file = request.files['file']
+            if file.filename == '':
+                flash('No selected file')
+                return redirect('http://127.0.0.1:5000/')
+            else:
+                file.save(file.filename)
+                return 'file uploaded successfully'
+    return redirect(request.url)
+     
 
 if __name__ == '__main__':
     app.run(debug = True)
